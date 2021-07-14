0e7e1f4a9b5289f08a183654e8e812da8a712348
Ronald Corona <Ronaldmcorona@gmail.com>
2021-07-14 08:50:01 -0700

    .

--- src/shared/QuadTree.ts
+++ src/shared/QuadTree.ts
@@ -24,6 +24,7 @@ class QuadTree implements Rectangle {
     }
 
     insert(point : Point) {
+        if (!this.contains(point)) return;
         if (this.points.length < this.levelCapacity)
         {
             this.points.push(point)
@@ -31,22 +32,80 @@ class QuadTree implements Rectangle {
         else
         {
             this.subdivide()
+            for (const d of dirs)
+            {
+                this.quadrant![d].insert(point)
+            }
         }
     }
 
     private subdivide() {
         if (this.quadrant) return;
+
         this.quadrant = dirs.reduce((a, d : Direction) => {
             const [dy, dx] = DIR[d]
             const x = this.x + dx * this.w/2
             const y = this.y + dy * this.h/2
             a[d] = new QuadTree(x, y, this.w/2, this.h/2, this.levelCapacity)
+            this.points.forEach(p => a[d].insert(p))
             return a
-            }, {} as Record<0|1|2|3, QuadTree>)
+        }, {} as Record<0|1|2|3, QuadTree>)
     }
 
     private contains(p : Point) {
         return this.x <= p.x && p.x <= this.x + this.w
             && this.y <= p.y && p.y <= this.y + this.h
     }
+
+    clear() {
+        this.points = []
+        this.quadrant = undefined
+    }
+
+    private static canvas = document.createElement("canvas")
+    private static ctx = QuadTree.canvas.getContext("2d")!
+    draw(isInner? : true) {
+        const W =  window.innerWidth
+        if (!isInner)
+        {
+            document.body.appendChild(QuadTree.canvas)
+            QuadTree.canvas.width = W
+            QuadTree.canvas.height = W
+            QuadTree.canvas.style.position = 'absolute'
+            QuadTree.canvas.style.top = '0'
+            QuadTree.canvas.style.left = '0'
+            QuadTree.canvas.style.background = 'yellow'
+            QuadTree.canvas.style.width = '100%'
+    
+            QuadTree.ctx.fillStyle = "white"
+            QuadTree.ctx.strokeStyle = "black"
+            QuadTree.ctx.lineWidth = 3
+            QuadTree.ctx.clearRect(0,0, W, W)
+        }
+        QuadTree.ctx.beginPath()
+        QuadTree.ctx.strokeRect(this.x * W, this.y * W, this.w * W, this.h * W)
+        QuadTree.ctx.closePath()
+        if (this.quadrant)
+        {
+            for (const d of dirs)
+            {
+                this.quadrant[d].draw(true)
+            }
+        }
+        else
+        {
+            QuadTree.ctx.strokeStyle = "green"
+            for (const { x, y } of this.points)
+            {
+                QuadTree.ctx.beginPath()
+                QuadTree.ctx.arc(x * W, y * W, 3, 0, 7)
+                QuadTree.ctx.closePath()
+                QuadTree.ctx.stroke()
+            }
+        }
+        QuadTree.ctx.strokeStyle = "black"
+        QuadTree.ctx.strokeRect(this.x * W, this.y * W, this.w * W, this.h * W)
+    }
 }
+
+Object.assign(globalThis, { QuadTree })
--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -7,7 +7,7 @@ const CONSTANTS = (() => {
     const PLAYER_SPEED = 0.0002
     
     const BULLET_COOLDOWN = 80 // 200
-    const BULLET_SPEED = 0.0006 * 2 // / 2
+    const BULLET_SPEED = 0.0006 / 2
     
     const FPS = 20 // 60
     const GAME_TICK = 1000 / FPS
