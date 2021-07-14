4963df8a590946995d7eb3cb32b03f52fe2164af
JomoPipi <ronaldmcorona@gmail.com>
2021-07-14 02:46:41 -0700

    .

--- src/shared/QuadTree.ts
+++ src/shared/QuadTree.ts
@@ -1,20 +1,28 @@
 
+const dirs = [0,1,2,3] as const
+const DIR = [[0,0],[0,1],[1,0],[1,1]] as const
+
+type Direction = typeof dirs[number]
+
 class QuadTree implements Rectangle { 
     x; y; w; h; levelCapacity;
     points : Point[] = []
+    quadrant? : Record<0|1|2|3, QuadTree>
+
     constructor(
         x : number, 
         y : number, 
-        width : number, 
-        height : number, 
+        w : number, 
+        h : number, 
         levelCapacity : number) {
             
         this.x = x
         this.y = y
-        this.w = width
-        this.h = height
+        this.w = w
+        this.h = h
         this.levelCapacity = levelCapacity;
     }
+
     insert(point : Point) {
         if (this.points.length < this.levelCapacity)
         {
@@ -22,8 +30,23 @@ class QuadTree implements Rectangle {
         }
         else
         {
-
-            this.points.push(point)
+            this.subdivide()
         }
+    }
+
+    private subdivide() {
+        if (this.quadrant) return;
+        this.quadrant = dirs.reduce((a, d : Direction) => {
+            const [dy, dx] = DIR[d]
+            const x = this.x + dx * this.w/2
+            const y = this.y + dy * this.h/2
+            a[d] = new QuadTree(x, y, this.w/2, this.h/2, this.levelCapacity)
+            return a
+            }, {} as Record<0|1|2|3, QuadTree>)
+    }
+
+    private contains(p : Point) {
+        return this.x <= p.x && p.x <= this.x + this.w
+            && this.y <= p.y && p.y <= this.y + this.h
     }
 }
--- src/shared/typehelpers.ts
+++ src/shared/typehelpers.ts
@@ -1,3 +1,4 @@
 
 type OmitByValue<V,T> = { [key in keyof T as T[key] extends V ? never : key] : T[key] }
 type PickByValue<V,T> = { [key in keyof T as T[key] extends V ? key : never] : T[key] }
+type ReadonlyRecord<K extends string | number | symbol, V> = { readonly [key in K] : V }
